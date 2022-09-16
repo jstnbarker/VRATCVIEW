b587f3f5c7f5617740c10b41fdde4e943d676b03
Justin <jstnbarker@pm.me>
2022-09-16 19:41:54 -0400

    API requests fufilled

--- API Model/API Model/API Model/Program.cs
+++ API Model/API Model/API Model/Program.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using System.Collections;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using Newtonsoft.Json; // Project -> Manage NuGet Packages -> Search for Newtonsoft
 using Newtonsoft.Json.Linq;
 
 namespace Program
 {
+    internal class airplane
+    {
+        public double z = 0;
+        public double x = 0;
+        public double y = 0;
+        public double heading = 0;
+
+        public airplane(double z, double y, double x, double heading)
+        {
+            this.z = z;
+            this.y = y;
+            this.heading = heading;
+            this.x = x;
+        }
+    }
     class Program
     {
+        static double latn = 34;
+        static double lonn = -84.61907;
         static async Task<string> apireq()
         {
             {
@@ -37,8 +57,37 @@ namespace Program
             var result = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonString);
             Console.WriteLine(result.Keys.ElementAt(0));
             var value = new object();
-            var ac = result.TryGetValue("ac", out value);
-            Thread.Sleep(10000);
+            object ac = new object();
+
+            if (result.TryGetValue("ac", out value))
+            {
+                ac = result["ac"];
+            }
+
+            ArrayList aircraft = new ArrayList();
+            foreach (var i in (JArray)ac)
+            {
+                double lattemp = (double)i["lat"] - latn;
+                double lontemp = (double)i["lon"] - lonn;
+                double alttemp = 0;
+                try
+                {
+                    alttemp = (double)i["alt_baro"];
+                }
+                catch (Exception e) { };
+
+                double headtemp = 0;
+                try
+                {
+                    headtemp = (double)i["track"];
+                }
+                catch(Exception e){};
+                
+
+                Console.WriteLine(lattemp + "  " + lontemp + "  " + alttemp + " " + headtemp);
+
+                aircraft.Add(new airplane(alttemp, lattemp, lontemp, headtemp));
+            }
         }
     }
 }
